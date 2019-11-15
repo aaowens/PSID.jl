7ba8003f4aa4d3e341c9a6b979cc0d43badae437
Andrew Owens <owensaa@gmail.com>
2019-11-14 21:13:01 -0500

    Improvements

--- src/PSID.jl
+++ src/PSID.jl
@@ -1,6 +1,6 @@
 module PSID
 using XLSX, DataDeps, DataFrames, CSV, LightXML, AbstractTrees, JSONTables, JSON3
-using DataFramesMeta, SHA, DataStructures, ProgressMeter
+using DataFramesMeta, SHA, DataStructures
 
 include("types.jl")
 include("init.jl")
